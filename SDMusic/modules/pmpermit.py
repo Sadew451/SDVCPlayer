a537b677f353a0392d4276b9d6bbf4b68e1578ca
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-10-05 20:21:54 +0530

    Update pmpermit.py

--- SDMusic/modules/pmpermit.py
+++ SDMusic/modules/pmpermit.py
@@ -18,7 +18,7 @@ async def pmPermit(client: USER, message: Message):
                 return
             await USER.send_message(
                 message.chat.id,
-                "Hi there, This is a music assistant service .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If you are sending a message here it means admin will see your message and join chat\n    - Don't add this user to secret groups.\n   - Don't Share private info here\n\n",
+                "Hi there, This is a Music Assistant Service .\n\n ❗️ Rules:\n   - No Chatting Allowed\n   - No Spam Allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If You Are Sending A Message Here it Means Admin Will See Your Message And Join Chat\n    - Don't Add This User To Secret Groups.\n   - Don't Share Private Info Here\n\n",
             )
             return
 

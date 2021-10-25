a289400d006707988edaf13e0e2a05f6023ab950
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-10-25 21:31:02 +0530

    Update youtube.py

--- SDMusic/services/downloaders/youtube.py
+++ SDMusic/services/downloaders/youtube.py
@@ -6,30 +6,20 @@ from SDMusic.config import DURATION_LIMIT
 from SDMusic.helpers.errors import DurationLimitError
 
 ydl_opts = {
-    "format": "bestaudio/best",
-    "verbose": True,
+    "format": "bestaudio[ext=m4a]",
     "geo-bypass": True,
     "nocheckcertificate": True,
     "outtmpl": "downloads/%(id)s.%(ext)s",
 }
-
 ydl = YoutubeDL(ydl_opts)
 
 
 def download(url: str) -> str:
     info = ydl.extract_info(url, False)
     duration = round(info["duration"] / 60)
-
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, "
-            f"the provided video is {duration} minute(s)",
+            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
         )
-    try:
-        ydl.download([url])
-    except:
-        raise DurationLimitError(
-            f"🛑 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, "
-            f"the provided video is {duration} minute(s)",
-        )
+    ydl.download([url])
     return path.join("downloads", f"{info['id']}.{info['ext']}")
